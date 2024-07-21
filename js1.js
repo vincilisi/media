df99b33d368fe0efc05de5f44a15923696269aea
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-21 15:32:08 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,8 @@
+let num1= 2;
+let num2= 45;
+let num3= 115;
+let num4= 62;
+let num5= 205;
+let somma= num1+num2+num3+num4+num5;
+let media= somma / 5;
+console.log("La somma tra i numeri è: " + somma + " e la media equivale a "+ media);
